44fd50663325ae9b3ec908fbc8ec0ef9125fb123
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-09-27 15:28:16 -0400

    Day 10 of 100 days of JavaScript complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import NetworkStatus from "./Components/NetworkStatus";
 
+
 function App() {
   const status = NetworkStatus();
   return (
-    <div className="App">
+    <div className={status ? "AppOn" : "AppOff"}>
       <h1>Internet Connection Status</h1>
+      {status ? <i class="bi bi-wifi"></i> : <i class="bi bi-wifi-off"></i>}
+
       <h2>{status ? "Online" : "Offline"}</h2>
     </div>
   );
